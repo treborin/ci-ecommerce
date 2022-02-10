0a8f9412f7c183c41e1c3560b908b59cbf6a4650
Josué <jmunganga039@gmail.com>
2022-02-10 12:29:28 +0200

    Added users migration

---
+++ app/Database/Migrations/2022-02-10-102631_AddUsers.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddUsers extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'firstname'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '50',
+            ],
+            'lastname'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '50',
+            ],
+            'email'       => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'is_admin' => [
+
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'default' => 'current_timestamp()',
+            ],
+            'updated_at'  => [
+                'type'  => 'DATETIME',
+                'default' => 'current_timestamp()',
+            ]
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
