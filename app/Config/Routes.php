0d01798cc04b85c1c8318695fc29097b085165e1
Josué <jmunganga039@gmail.com>
2022-02-11 10:10:22 +0200

    updating routes for profile page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Users::index');
+$routes->get('logout', 'Users::logout');
 $routes->match(["get", "post"],'/register', 'Users::register');
 $routes->match(["get", "post"],'/profile', 'Users::profile');
 
